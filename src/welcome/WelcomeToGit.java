627aa45470f6b86bfe4778ca6f98b0655591911a
priya <priya@LAPTOP-FF75FAJU.bbrouter>
2022-09-22 01:32:16 +0530

    welcome

---
+++ src/welcome/WelcomeToGit.java
@@ -0,0 +1,11 @@
+package welcome;
+
+public class WelcomeToGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to the GitRepository");
+
+	}
+
+}
